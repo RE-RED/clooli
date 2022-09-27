252927f3f6b8ada492a8adee2f620063c1ebb2af
gp2902 <rebeca.gp@aluno.ifsc.edu.br>
2022-09-27 17:21:51 +0000

    test

--- chatbot.py
+++ chatbot.py
@@ -24,48 +24,97 @@ estados = {
         }
     },
     2: {
-        'frases': ['??? \n - ...droga...! \n(Escreva "1" para continuar)'],
+        'frases': ['??? \n - ...droga...!'],
         'proximos_estados': {
-            '1': 3
+            '1': 3,
+            '2': 1
         }
     },
     3: {
-        'frases': ['??? \n - Esqueci de desligar... \n("1" para continuar)'],
+        'frases': ['??? \n - Esqueci de desligar...'],
         'proximos_estados': {
-            '1': 4
+            '1': 4,
+            '2': 2
         }
     },
     4: {
         'frases': ['??? \n - Talvez eu deva dormir novamente... \n(ESCOLHA: "Levantar" ou "Dormir"' ],
         'proximos_estados': {
             'Levantar': 5,
-            'Dormir': 6
+            'Dormir': 6,
+            '2': 3
         }
     },
     5: {
-        'frases': ['Você levanta e vai até a cozinha. \n??? \n - Bom dia mãe! \n("1" para continuar)'],
+        'frases': ['???\nVocê levanta e vai até a cozinha. \n??? \n - Bom dia mãe!'],
         'proximos_estados': {
-            '1': 8 
+            '1': 8, 
+            '2': 4
         }
     },
     6: {
-        'frases': ['... \n("1" para continuar)'],
+        'frases': ['???\n...'],
         'proximos_estados': {
-            '1': 7
+            '1': 7,
+            '2': 4
         }
     },
     7: {
-        'frases': ['??? \n -Que droooga, agora não consigo dormir mais. \n("1" para continuar)'],
+        'frases': ['??? \n -Que droooga, agora não consigo dormir mais.'],
         'proximos_estados': {
-            '1': 5
+            '1': 5,
+            '2': 6
         }
     },
     8: {
         'frases': ['Mãe\n - Bom dia!~'],
         'proximos_estados': {
-            '1': 1
+            '1': 9,
+            '2': 5
         }
-    }
+    },
+    9: {
+        'frases': ['???\nEla parece animada\n - Aconteceu alguma coisa?'],
+        'proximos_estados': {
+            '1': 10,
+            '2': 8
+        }
+    },
+    10: {
+        'frases': ['Mãe\n - Eu passei naquela entrevista e começo hoje'],
+        'proximos_estados': {
+            '1': 11,
+            '2': 9
+        }
+    },
+    11: {
+        'frases': ['???\n - Que bom! Vamos comemorar mais tarde?'],
+        'proximos_estados': {
+            '1': 12,
+            '2': 10
+        }
+    },
+    12: {
+        'frases': ['Mãe\n - Com certeza! Ah, mais uma coisa. Lucas, não chega muito tarde, sei que você se perde enquanto corre por ai'],
+        'proximos_estados': {
+            '1': 13,
+            '2': 11
+        }
+    },
+    13: {
+        'frases': ['Lucas\n - OK! Não vou chegar tarde... Hoje'],
+        'proximos_estados': {
+            '1': 14,
+            '2': 12
+        }
+    },
+    14: {
+        'frases': ['Lucas\nDe volta para o quarto. Você se arruma e vai correr'],
+        'proximos_estados': {
+            '1': 15,
+            '2': 13
+        }
+    },
 }
 
 # Dicionário com os estados correntes de cada jogador.
