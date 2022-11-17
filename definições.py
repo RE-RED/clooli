8c5ecdab6b2c5c7a425f4e870ccea1fcaa938d93
gp2902 <rebeca.gp@aluno.ifsc.edu.br>
2022-11-17 18:11:18 +0000

    tentativa de atualização

--- definições.py
+++ definições.py
@@ -134,7 +134,7 @@ estados = {
         }
     },
     19: {
-        'fras'inventario': set()es': ['Mas estranhamente a neblina se aproxima cada vez mais. Uma moça que estava perto acaba tendo contato'],
+        'frases': ['Mas estranhamente a neblina se aproxima cada vez mais. Uma moça que estava perto acaba tendo contato'],
         'proximos_estados': {
             '[sS]eguir?': 20,
             '[rR]etornar?': 18
